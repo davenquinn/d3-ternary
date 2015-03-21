24edcbc1dd437fbfcc408f97aa6fbaab8fc7b6c4
Daven Quinn <dev@davenquinn.com>
2015-03-21 02:16:23 -0700

    Increased readability of example script

--- examples/soil-types/main.js
+++ examples/soil-types/main.js
@@ -1,37 +1,35 @@
-var gotData, grat, ternary;
+graticule = d3.ternary.graticule()
+  .majorInterval(0.2)
+  .minorInterval(0.05);
 
-grat = d3.ternary.graticule().majorInterval(0.2).minorInterval(0.05);
-
-ternary = d3.ternary.plot().call(d3.ternary.scalebars()).call(d3.ternary.vertexLabels(["Clay", "Sand", "Silt"])).call(d3.ternary.neatline()).call(grat);
+ternary = d3.ternary.plot()
+  .call(d3.ternary.scalebars())
+  .call(d3.ternary.vertexLabels(["Clay", "Sand", "Silt"]))
+  .call(d3.ternary.neatline())
+  .call(graticule);
 
 d3.select('body').call(ternary);
 
-gotData = function(d) {
-  var data, paths;
+function gotData(d) {
+
   data = d3.entries(d).map(function(d) {
-    var v;
-    v = d.value.map(function(c) {
-      return [c.sand, c.silt, c.clay];
-    });
-    return {
-      type: d.key,
-      value: v
-    };
+    v = d.value.map(function(c) {return [c.sand, c.silt, c.clay]});
+    return {type: d.key, value:v };
   });
-  paths = ternary.plot().selectAll("path").data(data);
-  return paths.enter().append('path').attr({
-    d: function(d) {
-      return ternary.path(d.value);
-    },
-    "class": 'ternary-line',
-    id: function(d) {
-      return d.type.replace('-', ' ');
-    }
-  }).on('click', function(d) {
-    return console.log(this.id);
-  });
+
+  paths = ternary.plot()
+    .selectAll("path")
+    .data(data);
+
+  paths
+    .enter()
+      .append('path')
+      .attr({
+        d: function(d) {return ternary.path(d.value);},
+        class: 'ternary-line',
+        id: function(d) {return d.type.replace('-', ' ')}
+        })
+      .on('click', function(d) { console.log(this.id);});
 };
 
 d3.json('data.json', gotData);
-
-
